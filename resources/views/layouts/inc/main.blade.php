2b9f2d2fbb0720642677f7930f8ddfe998ed1cc1
Dio-Damar-Danendra-Portofolio <diodamar14102000@gmail.com>
2025-04-08 15:06:31 +0700

    Update

--- resources/views/layouts/inc/main.blade.php
+++ resources/views/layouts/inc/main.blade.php
@@ -41,10 +41,10 @@
 <body>
 
   <!-- ======= Header ======= -->
-  @include('sweetalert::alert')
 
   @include('layouts.inc.header')
   <!-- End Header -->
+  @include('sweetalert::alert')
 
   <!-- ======= Sidebar ======= -->
   @include('layouts.inc.sidebar')
@@ -96,6 +96,8 @@
 
   <!-- Template Main JS File -->
   <script src="{{ asset('assets/js/main.js') }}"></script>
+
+  @include('sweetalert::alert', ['cdn' => 'https://cdn.jsdelivr.net/npm/sweetalert2@9'])
 
 </body>
 
